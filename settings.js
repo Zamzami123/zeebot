6943c9c4c2b17ae7c61e9951d7caa61b70a6b99a
Zamzami123 <89690519+Zamzami123@users.noreply.github.com>
2022-06-05 22:19:41 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,16 +21,16 @@ global.auto_welcome_msg = true
 global.auto_leave_msg = true
 global.prefix = "."
 global.language = "ind"
-global.ownerNumberr = "62887435047326"
-global.ownerbot = ['62887435047326@s.whatsapp.net','918156874290@s.whatsapp.net'] // .owner
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×"
-global.botname = "Alphabot"
+global.ownerNumberr = "6285733566761"
+global.ownerbot = ['6285733566761@s.whatsapp.net','6285733566761@s.whatsapp.net'] // .owner
+global.ownername = "MAS ZAKI"
+global.botname = "BOT_STORE"
 global.thumb = "thumbnail_1.jpg"
 global.thumbnail ="thumbnail_2_1.jpg"
 global.fakethumb ="thumbnail_1.jpg"
 global.donasi ="donasi.jpg"
 global.captionpay ="Scan untuk melakukan pembayaran"
-global.fake = "「  ALPHABOT  」\nCreated By ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×"
+global.fake = "「  BOT STORE  」\nCreated By MAS ZAKI"
 global.alphaVersion = [2, 2143, 3] //WhatsApp web version
 
 /* Available type menu
